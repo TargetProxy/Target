1f6f66278165c50851efdeaf17a8f53e88ad351b
loafman1120 <kangjun1120@hotmail.com>
2026-08-25 19:51:10 +0800

    Generate Apple projects and icons

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_alone
+import screen_retriever_macos
+import shared_preferences_foundation
+import targetlib
+import tray_manager
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAlonePlugin.register(with: registry.registrar(forPlugin: "FlutterAlonePlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TargetlibPlugin.register(with: registry.registrar(forPlugin: "TargetlibPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
